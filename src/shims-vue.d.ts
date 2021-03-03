de9ecb41e05ed7c6c53ec0e82e42944878ddb808
Madhura <madhura.bhujbal@gmail.com>
2021-03-03 17:35:01 +0000

    Add comment to vue.d.ts file

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,4 @@
+// This is helper file for TS to give a better definition of .vue files. Do not modify this file
 /* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
