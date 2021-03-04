1c57213dc5637473d2fc36ed54bf28f1deef6c9e
Madhura <madhura.bhujbal@gmail.com>
2021-03-04 11:37:51 +0000

    Create a new file to practice TS fundamentals

---
+++ typrscriptFundamentals.js
@@ -0,0 +1,7 @@
+// Common JS datatypes: String, Boolean, Number, Array, Function, Object
+/*Additional datatypes provided by TS: 
+1. Any: essentially disables any type checking
+2. Tuple: They are fixed length arrays of predefined datatypes e.g: RGB values of colors [number, number, number]
+          Array cannot contain any less or any more elements than defined
+3. Enum: Allows you to define friendly names to a set of values. e.g: enum arrowKeys = {Up, Down, Right, Left}
+ */
